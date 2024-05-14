f299449a6cd406477e6321e22de18aa28116f0db
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-15 00:30:50 +0300

    Initial Commit

---
+++ RotatingRow/RotatingRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotatingRowApp.swift
+//  RotatingRow
+//
+//  Created by admin on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RotatingRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
